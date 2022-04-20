40e6b4cc81324b5076868883e54ea1a1e30d1d7b
Dinh-Van Colomban <van.colomban@gmail.com>
2022-04-20 20:28:41 +0200

    fix(config): fixes vue module rules

--- config/vue.js
+++ config/vue.js
@@ -5,4 +5,27 @@ module.exports = {
     './common',
   ],
   rules: {},
+  overrides: [
+    // extends module override to vue files
+    {
+      files: '**/*.vue',
+      plugins: ['stylelint-scss'],
+      rules: {
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': [
+          true,
+          {
+            ignoreAtRules: ['value'],
+          },
+        ],
+        'function-no-unknown': null,
+        'scss/function-no-unknown': [
+          true,
+          {
+            ignoreFunctions: ['global'],
+          },
+        ],
+      },
+    },
+  ],
 };
